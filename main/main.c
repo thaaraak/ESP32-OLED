242269f44a7cc3d21c4d42a5f9099d8961ec1e20
thaaraak <ghangrene@gmail.com>
2021-05-16 14:19:37 -0500

    A test

--- main/main.c
+++ main/main.c
@@ -1,16 +1,3 @@
-/* i2c - Example
-
-   For other examples please check:
-   https://github.com/espressif/esp-idf/tree/master/examples
-
-   See README.md file to get detailed usage of this example.
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
